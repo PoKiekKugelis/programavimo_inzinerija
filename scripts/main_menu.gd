b1c626641c9219f0ce8eb4cd0893aa9b78b6d70e
Velyvis <velyvismok@gmail.com>
2025-03-07 17:41:29 +0200

    Scrum-6 | Added Pause menu
    - Pause menu can be activated by pressing "escape" while in game.tscn scene
    - To unpause, press either: escape on keyboard, resume on screen, B on xbox controller
    - Made both Main menu and Pause menu fully traversable with keyboard and controller

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,10 @@
 extends Control
 
-
 var target_scene = "res://scenes/game.tscn" #path of main game scene
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	pass 
+	$ButtonContainer/Start.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,8 +12,6 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file(target_scene) #Simply changes the scene
-	pass
-	
+
 func _on_quit_pressed():
 	get_tree().quit() #Simply closes the game 
-	pass
