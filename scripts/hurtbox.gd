9f632c3f5f4bab2efb988150150cd9cf4c09fde0
PoKiekKugelis <maneig@ktu.lt>
2025-03-15 11:46:40 +0200

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit f515f581ab5c7cc8648ca639b294e47f21fbc4fa, reversing
    changes made to 72026ffb3c71c8da7ad080fcf3d9a40445497fcd.

--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -3,12 +3,7 @@ extends Area2D
 
 signal received_damage(damage: int)
 
-#naudojamas kad nusiust signala game scenai, su priliesto prieso informacija
-#player scena dalyvauja kaip tarpininkas
-signal enemy_touched(enemy: CharacterBody2D) 
-
 @export var health: Health
-
 
 func _ready():
 	connect("area_entered", _on_area_entered)
@@ -17,9 +12,3 @@ func _on_area_entered(hitbox: HitBox) -> void:
 	if hitbox != null:
 		health.health -= hitbox.damage
 		received_damage.emit(hitbox.damage)
-		
-		#Jeigu buvo priliestas entity is "enemy" grupes, pereinama i combat
-		if hitbox.owner.is_in_group("enemy"):
-			enemy_touched.emit(hitbox.owner)
-		
-	
